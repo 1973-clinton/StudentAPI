c0293a375e0b90c9fc0a0883f66e75235fbe9422
1973-clinton <clintonboamahofficial@gmail.com>
2022-06-07 22:53:37 +0000

    completed project setup and data access layer

---
+++ StudentAPI.Data/Context/ApplicationContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAPI.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAPI.Data.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(new Student
+            {
+                Id = 1,
+                FirstName = "Clinton",
+                LastName = "Boamah",
+                Email = "clinton.boamah@outlook.com",
+                Gender = Gender.Male,
+                DateJoined = DateTime.Now,
+                DateOfBirth = DateTime.Now,
+                Age = 30
+            });
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentAPI.Data/Migrations/20220607224027_firstMigration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace StudentAPI.Data.Migrations
+{
+    public partial class firstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Gender = table.Column<int>(type: "integer", nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DateJoined = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "DateJoined", "DateOfBirth", "Email", "FirstName", "Gender", "LastName" },
+                values: new object[] { 1, 30, new DateTime(2022, 6, 7, 22, 40, 26, 270, DateTimeKind.Local).AddTicks(2517), new DateTime(2022, 6, 7, 22, 40, 26, 270, DateTimeKind.Local).AddTicks(3023), "clinton.boamah@outlook.com", "Clinton", 0, "Boamah" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
